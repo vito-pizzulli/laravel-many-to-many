3fd2ba00b0952a5f86efc1216540537d5cb65f91
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-29 18:17:37 +0200

    Bonus 2 completed - now Types & Technologies can be managed directly from the admin panel, link to Technologies List index added to header navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,9 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand me-5" href="{{ route('admin.projects.index') }}">Projects List</a>
-            <a class="navbar-brand me-5" href="{{ route('admin.types.index') }}">Types List</a>
+            <a class="navbar-brand me-5 fs-3" href="{{ route('admin.projects.index') }}">Projects</a>
+            <a class="navbar-brand me-5" href="{{ route('admin.types.index') }}">Types</a>
+            <a class="navbar-brand" href="{{ route('admin.technologies.index') }}">Technologies</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
