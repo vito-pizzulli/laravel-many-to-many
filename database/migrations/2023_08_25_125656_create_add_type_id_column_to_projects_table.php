f0cb827ca5f13f7d82e8f3dac4d7619a90687cae
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-29 09:57:12 +0200

    added onUpdate & onDelete behavior for type_id column in projects table, set nullable

--- database/migrations/2023_08_25_125656_create_add_type_id_column_to_projects_table.php
+++ database/migrations/2023_08_25_125656_create_add_type_id_column_to_projects_table.php
@@ -12,10 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigInteger('type_id')->after('id');
+            $table->unsignedBigInteger('type_id')->after('id')->nullable();
             $table->foreign('type_id')
                 ->references('id')
-                ->on('types');
+                ->on('types')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
         });
     }
 
