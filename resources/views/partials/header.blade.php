732cf9be8ecda5ca4c78907b70c5374082d13c42
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-29 11:03:04 +0200

    removed projects create & projects recycle bin links from header, added types list index link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,8 +2,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand me-5" href="{{ route('admin.projects.index') }}">Projects List</a>
-            <a class="navbar-brand me-5" href="{{ route('admin.projects.create') }}">Add New Project</a>
-            <a class="navbar-brand" href="{{ route('admin.projects.trashed') }}">Recycle Bin</a>
+            <a class="navbar-brand me-5" href="{{ route('admin.types.index') }}">Types List</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
