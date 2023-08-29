5ee0fb457705793e993142d90e66e2434255257b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-29 18:14:34 +0200

    added SoftDeletes, fillables & slug as route key in Technology Model

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Technology extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 
     public function projects() {
         return $this->belongsToMany(Project::class);
